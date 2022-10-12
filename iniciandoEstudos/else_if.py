a586f5e726a50f59e6ed5e17096b016fecec7699
francienevaz <fran.vfraga@gmail.com>
2022-10-12 09:53:43 -0300

    updated else_if

--- iniciandoEstudos/else_if.py
+++ iniciandoEstudos/else_if.py
@@ -14,4 +14,31 @@ else:
     print("Ainda não pode tirar a CNH")
 
 
-#  elif é usado para estabelecer várias condições em python, é como se fosse o else if em Javascript 
+#  elif é usado para estabelecer várias condições em python, é como se fosse o else if em Javascript
+# If aninhado:
+
+conta_normal = True
+conta_universitaria = False
+
+saldo = 2000
+saque = 500
+cheque_especial = 450
+status= "Sucesso" if saldo >= saque else "Falha" # Essa é uma condição ternária em Python - if ternário 
+# em JS saldo >= saque ? "Sucesso" : "Falha"
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial!")
+    else:
+        print("Não foi possível realizar o saque, saldo insuficiente!")
+
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
+    
+else:
+    print("Nosso sistema não reconheceu o seu tipo de conta, por favor entre em contato com o seu gerente!")
