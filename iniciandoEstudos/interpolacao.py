dee26b2547fdbc62b311595af5c9883003c7e16c
francienevaz <fran.vfraga@gmail.com>
2022-10-16 19:55:59 -0300

    add interpolacao file

---
+++ iniciandoEstudos/interpolacao.py
@@ -0,0 +1,11 @@
+# Old style %
+
+nome = "Fran"
+idade = 28
+profissao = "Dev Frontend"
+linguagem = "Javascript"
+
+# Uma forma mais antiquada de fazer a interpolação de variaáveis é usando o %s e %d. No exemplo abaixo é só substituir as chaves por %s para string e %d para o número
+
+# Método format
+print("Olá, me chamo {}. Eu tenho {} anos de idade, trabalho como {} e estou matriculado no curso de {}" .format(nome, idade, profissao, linguagem))
